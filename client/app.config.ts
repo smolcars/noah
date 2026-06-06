fc8d6d35d30f56a7860493a9c1b0373d3179ab8b
Hampus Sjöberg <hampus.sjoberg@protonmail.com>
2026-06-06 16:44:50 +0200

    Version 0.0.18

--- client/app.config.ts
+++ client/app.config.ts
@@ -124,8 +124,8 @@ const config: { expo: ExpoConfig } = {
 };
 
 // WARNING: Do not change these values manually. Use the `just bump` command instead.
-config.expo.version = "0.0.17";
-config.expo.android!.versionCode = 19;
-config.expo.ios!.buildNumber = "19";
+config.expo.version = "0.0.18";
+config.expo.android!.versionCode = 20;
+config.expo.ios!.buildNumber = "20";
 
 export default config;
